101e38ac46b43914cac4ea3553b5dfd1ad81c5b9
Saif Ali <saifali7646@gmail.com>
2021-08-03 01:32:33 +0000

    added /urls/new end point to get POST requests

--- express_server.js
+++ express_server.js
@@ -5,10 +5,20 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
+//Library will convert the request body from a Buffer into string that we can read.
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -27,6 +37,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
