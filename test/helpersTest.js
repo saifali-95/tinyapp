6bea53e9eaf048ba771d242e32a62e9d81b2efe3
Saif Ali <saifali7646@gmail.com>
2021-08-07 02:41:25 +0000

    initial release of test cases

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedOutput = testUsers.userRandomID;
+    // Write your assert statement here
+    assert.deepEqual(user, expectedOutput);
+  });
+
+  it('should return false for the user not in the database', function() {
+    const user = getUserByEmail("abc@example.com", testUsers)
+    const expectedOutput = false;
+    // Write your assert statement here
+    assert.deepEqual(user, expectedOutput);
+  });
+});
